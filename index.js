18fed43a564aade31ff96585d521dd1815919392
sithusss <74533456+sithusss@users.noreply.github.com>
2021-08-26 13:42:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function myfunction(){
+    window.open("index.html");
+}
+function apply(){
+    window.open("apply.html")
+}
+function about(){
+    window.open("about.html")
+}
+function home(){
+    window.open("index.html")
+}
